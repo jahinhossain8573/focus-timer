ef8816d8c22f696a2e91c90354eb9e30c894c66b
Md Jahin Hossain <jahin.hossain2006@gmail.com>
2026-08-30 18:09:06 +0600

    Created a basic timer with start, stop and reset functionality and a total time display in the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-slate-950`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,82 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState, useRef, useEffect } from "react";
+
+export default function App() {
+  const [isRunning, alterIsRunning] = useState(false);
+  const [elapsedTime, alterElapsedTime] = useState(0);
+  const [totalTime, alterTotalTime] = useState(0);
+  const initialTime = useRef(0);
+  const intervalID = useRef(null);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalID.current = setInterval(() => {
+        alterElapsedTime(Date.now() - initialTime.current);
+      }, 1000);
+    } else {
+      clearInterval(intervalID.current);
+    }
+  }, [isRunning]);
+
+  function onPressed() {
+    alterIsRunning(!isRunning);
+    if (isRunning === false) {
+      initialTime.current = Date.now() - elapsedTime;
+    }
+  }
+
+  function onReset() {
+    alterElapsedTime(0);
+    alterTotalTime(totalTime + elapsedTime);
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div className="p-4 gap-5">
+      <header className="flex justify-between items-center text-amber-200">
+        <h1 className="text-4xl">
+          <b>Pomodoro Timer</b>
+        </h1>
+        <span className="text-2xl">
+          Hours Focused: {Math.trunc(totalTime / 1000 / 60).toString()}:
+          {(Math.trunc(totalTime / 1000) % 60).toString()}
+        </span>
+      </header>
+      <div className="text-amber-50 text-center p-10">
+        <h2 className="p-2 text-2xl">
+          {Math.trunc(elapsedTime / 1000 / 60).toString()}:
+          {(Math.trunc(elapsedTime / 1000) % 60).toString()}
+        </h2>
+        {isRunning ? (
+          <>
+            <button
+              className="border border-amber-50 px-2 py-1 rounded-2xl"
+              onClick={onPressed}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              Stop Timer
+            </button>
+          </>
+        ) : (
+          <div className="flex gap-4 justify-center">
+            <button
+              className="border border-amber-50 px-2 py-1 rounded-2xl"
+              onClick={onPressed}
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+              Start Timer
+            </button>
+            {elapsedTime === 0 ? (
+              <></>
+            ) : (
+              <button
+                className="border border-amber-50 px-2 py-1 rounded-2xl"
+                onClick={onReset}
+              >
+                Reset Timer
+              </button>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
